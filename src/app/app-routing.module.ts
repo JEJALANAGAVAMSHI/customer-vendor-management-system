5514a107b51ad45ab3d3258933c0a4b96e1e2125
Jejala.Nagavamshi <jejala.nagavamshi@ust.com>
2024-09-03 17:20:18 +0530

    Added logincomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BusinessRegisterComponent } from './components/businessregister/businessregister.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'businessregister', component: BusinessRegisterComponent }
+  { path: 'businessregister', component: BusinessRegisterComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { FormsModule } from '@angular/forms';
 import { BusinessRegisterComponent } from './components/businessregister/businessregister.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { BusinessRegisterComponent } from './components/businessregister/busines
     NavbarComponent,
     FooterComponent,
     HomeComponent,
-    BusinessRegisterComponent
+    BusinessRegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
