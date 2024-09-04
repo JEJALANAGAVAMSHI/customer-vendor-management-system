98f47ce26f6e2fec1c2c012ecf1e04a50f5f5f77
SharonJoseph.Enduri <SharonJoseph.Enduri@ust.com>
2024-09-04 17:46:58 +0530

    created RegisterUser model

---
+++ src/app/models/registerCustomer.ts
@@ -0,0 +1,9 @@
+export interface RegisterCustomer{
+    userName : string,
+    email : string,
+    password : string,
+    phoneNumber : string,
+    address : string,
+    state : string,
+    postalCode : string
+}
--- src/app/services/register-customer.service.ts
+++ src/app/services/register-customer.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthResponseDto } from '../models/authResponseDto';
+import { RegisterCustomer } from '../models/registerCustomer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterCustomerService {
-
-  constructor() { }
+  baseUrl:string = 'http://localhost:5000/api/auth/register-customer'
+  constructor(private http:HttpClient) { }
+  public RegisterCustomer = ( body: RegisterCustomer) => {
+    return this.http.post<AuthResponseDto>(this.baseUrl, body);
+  }
 }
