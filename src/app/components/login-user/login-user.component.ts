43f9711919b6dfc125aff3a0e5ced2648865931f
SharonJoseph.Enduri <SharonJoseph.Enduri@ust.com>
2024-09-04 09:23:42 +0530

    added authResponse dto model

--- src/app/components/login-user/login-user.component.ts
+++ src/app/components/login-user/login-user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginUser } from '../../models/login-user';
+import { LoginUserService } from '../../services/login-user.service';
 
 @Component({
   selector: 'app-login-user',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './login-user.component.css'
 })
 export class LoginUserComponent {
+  user : LoginUser={
+    username:  '',
+    email: '',
+    password: ''
+  }
 
+  constructor(private authService: LoginUserService) { }
+
+  onSubmit() {
+    this.authService.loginUser(this.user)
+      .subscribe(
+        response => {
+          console.log('Registration successful');
+          console.log('Token:', response.token);
+        },
+        error => {
+          console.error('Registration failed', error);
+        }
+      );
+  }
 }
--- src/app/services/login-user.service.ts
+++ src/app/services/login-user.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthResponseDto } from '../models/authResponseDto';
 import { LoginUser } from '../models/login-user';
 
 @Injectable({
@@ -9,6 +10,6 @@ export class LoginUserService {
   baseUrl:string = 'http://localhost:5000/api/auth/login'
   constructor(private http:HttpClient) { }
   public loginUser = ( body: LoginUser) => {
-    return this.http.post<LoginUser>(this.baseUrl, body);
+    return this.http.post<AuthResponseDto>(this.baseUrl, body);
   }
 }
