edd84732c185cb50cf29fcdb1a889998b9619e02
Jejala.Nagavamshi <jejala.nagavamshi@ust.com>
2024-09-05 17:23:53 +0530

    Created customerdashboard and vendordashboard components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddBusinessComponent } from './components/addbusiness/addbusiness.component';
 import { AdmindashboardComponent } from './components/admindashboard/admindashboard.component';
 import { BusinessRegisterComponent } from './components/businessregister/businessregister.component';
+import { CustomerdashboardComponent } from './components/customerdashboard/customerdashboard.component';
 import { CustomerregisterComponent } from './components/customerregister/customerregister.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { VendordashboardComponent } from './components/vendordashboard/vendordashboard.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -13,6 +15,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'addbusiness', component: AddBusinessComponent },
   { path: 'admindashboard', component: AdmindashboardComponent },
+  { path: 'vendordashboard', component: VendordashboardComponent },
+  { path: 'customerdashboard', component: CustomerdashboardComponent },
   { path: 'customerregister', component: CustomerregisterComponent }
 ];
 
---
+++ src/app/components/customerdashboard/customerdashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customerdashboard',
+  templateUrl: './customerdashboard.component.html',
+  styleUrl: './customerdashboard.component.css'
+})
+export class CustomerdashboardComponent {
+
+}
