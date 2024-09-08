a5c49067990def85e30ace284ebfe4c2115fd63b
SharonJoseph.Enduri <SharonJoseph.Enduri@ust.com>
2024-09-08 19:46:02 +0530

    added functionality to view all vendors

---
+++ src/app/components/view-vendors/view-vendors.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { catchError, map, of } from 'rxjs';
+import { VendorDto } from '../../models/vendorDto';
+import { ViewVendorsService } from '../../services/view-vendors.service';
+
+@Component({
+  selector: 'app-view-vendors',
+  templateUrl: './view-vendors.component.html',
+  styleUrl: './view-vendors.component.css'
+})
+export class ViewVendorsComponent implements OnInit {
+  vendors: VendorDto[] = [];
+  loading = true;
+  error: string | null = null;
+
+  constructor(private vendorsService: ViewVendorsService) {}
+
+  ngOnInit(): void {
+    this.loadVendors();
+  }
+  loadVendors(): void {
+    this.vendorsService.getAllVendors().pipe(
+      map((response: { vendors: VendorDto[] }) => {
+        // Extract the vendors from the vdjendors key
+        return response.vendors;
+      }),
+      catchError((error) => {
+        console.error('Error fetching vendors:', error);
+        this.error = 'Failed to load vendors. Please try again later.';
+        return of([]);
+      })
+    ).subscribe({
+      next: (vendors: VendorDto[]) => {
+        this.vendors = vendors;
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
+
+}
