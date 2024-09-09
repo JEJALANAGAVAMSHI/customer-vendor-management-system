6c36310e7438eafed8ad576c4640052b3df29e18
SharonJoseph.Enduri <SharonJoseph.Enduri@ust.com>
2024-09-09 19:54:20 +0530

    added addbusiness dto

---
+++ src/app/models/addBusinessDto.ts
@@ -0,0 +1,12 @@
+export interface AddBusinessDto {
+    businessName: string;
+    category: string;
+    address: string;
+    state: string;
+    postalCode: string;
+    email: string;
+    dayFrom: string;
+    dayTo: string;
+    timeFrom: string;
+    timeTo: string;
+  }
