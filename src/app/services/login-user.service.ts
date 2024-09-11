c650cceefd438351343545432535fa43da12a821
SharonJoseph.Enduri <SharonJoseph.Enduri@ust.com>
2024-09-11 13:46:39 +0530

    implemented guards

--- src/app/services/login-user.service.ts
+++ src/app/services/login-user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, Observable, tap, throwError } from 'rxjs';
+import { ApiResponse } from '../models/apiResponse';
 import { LoginUser } from '../models/login-user';
 
 @Injectable({
@@ -10,44 +11,50 @@ import { LoginUser } from '../models/login-user';
 export class LoginUserService {
 
   private baseUrl = 'http://localhost:5000/api/auth'; 
-
+  private tokenKey = 'token';
+  private roleKey = 'userRole';
   private role = '';
+  
 
   constructor(private http: HttpClient, private router: Router) {}
 
   loginUser(body: LoginUser): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/login`, body)
       .pipe(
-        tap(response => {
-          if (response && response.statusCode==200 && response.value && response.value.token) {
-            localStorage.setItem('token', response.value.token); 
-            this.role = response.value.roles[0]
-            this.redirectBasedOnRole(response.value.roles[0]); 
-          }else if (response.statusCode === 404) {
-            throw new Error('User Not Found');
-          } else if (response.statusCode === 400) {
-            throw new Error('Invalid Credentials');
-          } else {
-            throw new Error(response.message || 'An unexpected error occurred');
-          }
-        }),
+        tap(response => this.handleLoginSuccess(response)),
         catchError(this.handleError)
       );
   }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
   logout() {
-    // Clear user session data, tokens, etc.
-    localStorage.removeItem('userToken');  // Example: remove token from localStorage
-    // You can also clear other session data or cookies if necessary
+    
+    localStorage.removeItem('token');
+    localStorage.removeItem('userRole') 
+    this.router.navigate(['/login']);
   }
 
 
-  getUserRoleFromToken(token:string): string | null {
+  getUserRoleFromToken(): string | null {
     
-    return this.role;
+    return localStorage.getItem(this.roleKey);
     
   }
 
-  private redirectBasedOnRole(role: string): void {
+  private handleLoginSuccess(response:any): void {
+    if (response?.value?.token) {
+      localStorage.setItem(this.tokenKey, response.value.token);
+      const role = response.value.roles[0];
+      localStorage.setItem(this.roleKey, role);
+      this.redirectBasedOnRole(role);
+    }
+  }
+
+  public redirectBasedOnRole(role: string | null): void {
+    
     switch (role) {
       case 'Admin':
         this.router.navigate(['/admindashboard']);
@@ -56,6 +63,7 @@ export class LoginUserService {
         this.router.navigate(['/vendordashboard']);
         break;
       case 'Customer':
+        console.log('inside customer')
         this.router.navigate(['/customerdashboard']);
         break;
       default:
